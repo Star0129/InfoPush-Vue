aa8cb090c8d6f2f1372abbd8e90fb96eb6e9df20
Star <318511992@qq.com>
2021-02-10 21:45:39 +0800

    第一次提交

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        background-color="#2c3e50"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        :collapse="isCollapse"
+      >
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  props: [
+    'isCollapse'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <sidebar class="sidebar-container" :isCollapse="isCollapse"/>
+    <div class="main-container">
+      <navbar :isActive="isCollapse" @sl="sl"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Navbar, Sidebar } from './components'
+
+export default {
+  name: 'index',
+  data () {
+    return {
+      isCollapse: false
+    }
+  },
+  components: {
+    Navbar,
+    Sidebar
+  },
+  methods: {
+    sl: function () {
+      this.isCollapse = !this.isCollapse
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main-container {
+  min-height: 100%;
+  transition: margin-left .28s;
+  margin-left: 200px;
+  position: relative;
+}
+
+.sidebar-container {
+  transition: width 0.28s;
+  width:200px !important;
+  background-color: #2c3e50;
+  height: 100%;
+  position: fixed;
+  font-size: 0px;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1001;
+  overflow: hidden;
+}
+
+.scrollbar-wrapper {
+  overflow-x: hidden !important;
+}
+
+.el-scrollbar__bar.is-vertical {
+  right: 0px;
+}
+
+.el-scrollbar {
+  height: 100%;
+}
+
+.el-scrollbar {
+  height: calc(100% - 50px);
+}
+
+.is-horizontal {
+  display: none;
+}
+
+a {
+  display: inline-block;
+  width: 100%;
+  overflow: hidden;
+}
+
+.svg-icon {
+  margin-right: 16px;
+}
+
+.sub-el-icon {
+  margin-right: 12px;
+  margin-left: -2px;
+}
+
+.el-menu {
+  border: none;
+  height: 100%;
+  width: 100% !important;
+}
+</style>
